12278c545f0591f6f00bab307e0f2ff0796405d4
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-09-04 13:32:48 +0200

    Delete 1.kt

--- 1.kt
+++
@@ -1,3 +0,0 @@
-Street street = new Street(12, (byte) 1, "Oxford street");
-Address address = new Address(street, "123456", "London", "United Kingdom");
-Person person = new Person("Foo", "Bar", "foo.bar@gmail.com", Gender.MALE, address);
